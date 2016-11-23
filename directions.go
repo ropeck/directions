bb3078e3efa7b242e61d5466d93e934fa44bc994
Rodney Peck <ropeck@google.com>
2016-11-23 12:13:34 -0800

    google format

--- directions.go
+++ directions.go
@@ -1,37 +1,39 @@
 package directions
+
 import (
-	"os"
-	"time"
 	"html/template"
 	"net/http"
-        "google.golang.org/appengine"
-        "google.golang.org/appengine/datastore"
-        "google.golang.org/appengine/urlfetch"
-	"googlemaps.github.io/maps"
+	"os"
+	"time"
+
 	"github.com/kr/pretty"
-//        "golang.org/x/net/context"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/urlfetch"
+	"googlemaps.github.io/maps"
 )
 
 type Config struct {
-	Name string
+	Name  string
 	Value string
-  }
+}
 
 type Directions struct {
 	Origin string
-  Client *maps.Client
-  Apikey string
-  r *http.Request
-  Resp string
-  Leg *maps.Leg
-  Dir *maps.Route
-  Steps []*Step
+	Client *maps.Client
+	Apikey string
+	r      *http.Request
+	Resp   string
+	Leg    *maps.Leg
+	Dir    *maps.Route
+	Steps  []*Step
 }
 
 type Step struct {
-  Dist string
-  Duration time.Duration
-  Directions template.HTML
+	Dist       string
+	Duration   time.Duration
+	Directions template.HTML
 }
 
 func (d *Directions) GetApikey() string {
@@ -42,41 +44,40 @@ func (d *Directions) GetApikey() string {
 
 	c := os.Getenv("APIKEY")
 	for _, v := range res {
-	  if v.Name == "APIKEY" {
-	    c = v.Value
-	  }
-        }
+		if v.Name == "APIKEY" {
+			c = v.Value
+		}
+	}
 	return c
 }
 
 func NewDirections(r *http.Request) *Directions {
-  var d = new(Directions)
-  d.r = r
-  d.Apikey = d.GetApikey()
-  ctx := appengine.NewContext(r)
-  uc := urlfetch.Client(ctx)
-  c, err := maps.NewClient(maps.WithAPIKey(d.Apikey), maps.WithHTTPClient(uc))
-  d.Client = c
-  if err != nil {
-    d.Resp = err.Error()
-  }
-  return d
+	var d = new(Directions)
+	d.r = r
+	d.Apikey = d.GetApikey()
+	ctx := appengine.NewContext(r)
+	uc := urlfetch.Client(ctx)
+	c, err := maps.NewClient(maps.WithAPIKey(d.Apikey), maps.WithHTTPClient(uc))
+	d.Client = c
+	if err != nil {
+		d.Resp = err.Error()
+	}
+	return d
 }
 
 func (d *Directions) Directions() {
-  r := &maps.DirectionsRequest{
-      Mode:	   maps.TravelModeDriving,
-      Origin:      "1200 Crittenden Lane, Mountain View",
-      Destination: "90 Enterprise Way, Scotts Valley",
-  }
-  if d.Resp == "" {
-    resp, _, _ := d.Client.Directions(appengine.NewContext(d.r), r)
-    d.Dir = &resp[0]
-    for _, v := range d.Dir.Legs[0].Steps {
-      d.Steps = append(d.Steps, &Step{v.Distance.HumanReadable, v.Duration, template.HTML(v.HTMLInstructions)})
-    }
-    d.Resp = pretty.Sprint(d.Steps)
-    d.Leg = d.Dir.Legs[0]
-  } 
+	r := &maps.DirectionsRequest{
+		Mode:        maps.TravelModeDriving,
+		Origin:      "1200 Crittenden Lane, Mountain View",
+		Destination: "90 Enterprise Way, Scotts Valley",
+	}
+	if d.Resp == "" {
+		resp, _, _ := d.Client.Directions(appengine.NewContext(d.r), r)
+		d.Dir = &resp[0]
+		for _, v := range d.Dir.Legs[0].Steps {
+			d.Steps = append(d.Steps, &Step{v.Distance.HumanReadable, v.Duration, template.HTML(v.HTMLInstructions)})
+		}
+		d.Resp = pretty.Sprint(d.Steps)
+		d.Leg = d.Dir.Legs[0]
+	}
 }
-
